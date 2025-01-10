bfb2f21105a566ff4b78e5b0ebd7057464d9bb60
KaeL <uberkael@gmail.com>
2025-01-10 21:55:56 +0100

    Main Refactor

--- Main.py
+++ Main.py
@@ -11,7 +11,7 @@ def show_file_error():
     messagebox.showerror("Error", "File not found.")
 
 
-def show_error_alert():
+def show_empty_alert():
     messagebox.showerror("Error",
                          f"No data found in the backup file {Utils.BACKUP_FILE}")
 
@@ -29,6 +29,7 @@ def show_success_import(path):
 
 
 def importar():
+    """Importa el JSON backup en el navegador seleccionado"""
     path = Locations.get_browser_path(bw_sel.get().strip())
     file_path = filedialog.askopenfilename(initialfile="Web Data",
                                            initialdir=path,
@@ -41,14 +42,18 @@ def importar():
     print(f"Importing from {file_path}")
     filas = Utils.json_read(Utils.BACKUP_FILE)
     if len(filas) == 0:
-        show_error_alert()
+        show_empty_alert()
         return
 
-    Utils.db_insertar_filas(file_path, filas)
-    show_success_import(file_path)
+    try:
+        Utils.db_insertar_filas(file_path, filas)
+        show_success_import(file_path)
+    except Exception as e:
+        messagebox.showerror("Error", f"{e}")
 
 
 def exportar(bw_sel):
+    """Exporta Search Engines del navegador seleccionado en un archivo JSON"""
     path = Locations.get_browser_path(bw_sel.get().strip())
     file_path = filedialog.askopenfilename(initialfile="Web Data",
                                            initialdir=path,
@@ -63,7 +68,7 @@ def exportar(bw_sel):
     show_success_export()
 
 
-def select_bw():
+def select_browser():
     print(f"Browser: {bw_sel.get()}")
 
 
@@ -84,7 +89,9 @@ label = tk.Label(win, text="Close Browser before import or export")
 label.pack(pady=10)
 
 bws = Utils.add_spaces(Locations.LOCATIONS.keys())
-menu = ttk.OptionMenu(frame, bw_sel, bws[0], *bws, command=lambda _: select_bw)
+menu = ttk.OptionMenu(
+    frame, bw_sel, bws[0], *bws, command=lambda _: select_browser
+)
 menu.config(padding=(50, 5))
 menu.pack(anchor=tk.W)
 
@@ -96,7 +103,9 @@ btn_export = ttk.Button(
 )
 btn_export.pack(side=tk.LEFT, padx=5)
 
-btn_import = ttk.Button(frame_botones, text="Import into Browser", command=importar)
+btn_import = ttk.Button(
+    frame_botones, text="Import into Browser", command=importar
+)
 btn_import.pack(side=tk.RIGHT, padx=5)
 
 win.mainloop()
